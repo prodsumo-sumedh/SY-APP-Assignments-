c2141ab8e6fbe0f02c780dbfaef0c9811325c4c0
Sumedh Dhoke <clashersumedh@gmail.com>
2026-07-26 23:08:29 +0530

    Add files via upload

---
+++ Different_Designing_Methods.py
@@ -0,0 +1,27 @@
+# Observer Pattern
+class Subject:
+   def __init__ (self):
+      self._Observers = []
+
+   def attach(self, observer): 
+      self._Observers.append (observer)
+
+   def detach(self, observer): 
+      self._Observers.remove(observer)
+
+   def notify(self, message):
+      for observer in self._Observers:
+         observer.update(message)
+
+class Observer:
+   def update(self, message):
+       print(f"Recieved message: {message}" )
+
+#Usage
+subject = Subject()
+observer1 =Observer()
+observer2 = Observer()
+
+subject.attach(observer1) 
+subject.attach(observer2)
+subject.notify("Hello Observers!")
