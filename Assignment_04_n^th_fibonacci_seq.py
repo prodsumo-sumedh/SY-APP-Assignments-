1caf6fdc7d0bed24b88e0affab1761592089388e
Sumedh Dhoke <clashersumedh@gmail.com>
2026-07-30 22:28:54 +0530

    Update Assignment_04_n^th_fibonacci_seq.py

--- Assignment_04_n^th_fibonacci_seq.py
+++ Assignment_04_n^th_fibonacci_seq.py
@@ -8,6 +8,8 @@ def fib(n):
     return dp[n]
 print(f"The fibonacci number is : {fib(10)}")
 
+#Output
+The fibonacci number is : 55
 
 # Function to find the n-th Fibonacci number using Memorization (Dynamic Programming)
 memo = {}
@@ -20,3 +22,6 @@ def fib_s(n):
     return memo[n]
 
 print(f"The fibonacci number is :{fib(15)} ")
+
+#Output
+The fibonacci number is :610 
