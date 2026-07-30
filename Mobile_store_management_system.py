f80e898702d2372549be21c592dfe133897e84f1
Sumedh Dhoke <clashersumedh@gmail.com>
2026-07-30 23:56:00 +0530

    Add files via upload

---
+++ Mobile_store_management_system.py
@@ -0,0 +1,40 @@
+class Mobile:
+    def __init__(self,brand,model,price):
+        self.brand = brand
+        self.model = model
+        self.price = price
+
+    def Mob_Category(self):
+        if self.price >=60000:
+            return "Premium Category"
+        elif self.price >=25000 and self.price<60000:
+            return "Mid - range category"
+        else:
+            return "Budget"
+
+    def Display(self):
+        print(f"{self.brand:<10} {self.model:<15} {self.price:<12} {self.Mob_Category()}")
+
+class Store:
+    def __init__(self,name):
+        self.name = name
+        self.mobiles =[]
+
+
+    def Add_mobiles(self,brand,model,price):
+        mobile = Mobile(brand,model,price)
+        self.mobiles.append(mobile)
+        print(f"Mobile added successfully: {brand} {model}")
+
+    def display(self):
+        print(f"========== {self.name} =========")
+        print(f"{'Brand':<10} {'Model':<15} {'Price':<12} {'Category'}")
+        for mob in self.mobiles:
+            mob.Display()
+
+store = Store("Arnav mobile store")
+store.Add_mobiles("Apple","Iphone 17 pro",95000)
+store.Add_mobiles("samsung","F23",35000)
+store.Add_mobiles("Mototrola","g57 power",19000)
+
+store.display()
